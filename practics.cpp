3f8ad1d2f8ddc31367be717e0b69d8ccd7beedeb
SoumyadipCoder <soumyadip.edu2008@gmail.com>
2026-04-29 11:38:53 +0530

    Added practics.cpp

---
+++ practics.cpp
@@ -0,0 +1,82 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+
+//sum of n number
+	/*
+	int n;
+	cout<<"Enter a integer number:-";
+	cin>>n;
+	int i=1;
+	int sum=0;
+	while(i<=n){
+		sum=sum+i;
+		i++;
+	}
+	cout<<"Sum is =>"<<sum<<endl;
+	return 0;
+
+*/
+
+//if_else condition 
+
+/*	double n;
+	cout<<"Enter your bugget:-";
+	cin>>n;
+	if(n<50){
+		cout<<"Eat biscits.";
+	}
+	else if(50<=n<=100){
+		cout<<"Eat Roll.";	
+	}
+	else{
+		cout<<"Eat MOMO.";
+	}
+*/
+
+/*
+	
+    *
+	**
+	*** ... 
+	
+	int n;
+	cout<<"Enter an int number:-";
+	cin>>n;
+	for(int j=1;j<=n;j++){
+		for(int i=1; i<=j;i++){
+			cout<<"*";
+		}
+		cout<<"\n";
+	}
+*/
+
+
+/*  ***
+	**
+	*    
+	int n;
+	cout<<"Enter an int number:-";
+	cin>>n;
+	for(int j=n;j>=1;j--){
+		for(int i=j; i>=1;i--){
+			cout<<"*";
+		}
+		cout<<"\n";
+	}
+*/
+
+
+
+
+
+
+
+
+}
+
+
+
+
