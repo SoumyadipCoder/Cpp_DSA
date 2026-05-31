58447fa7d2628ff999d05d3b64444c67db6ca02d
SoumyadipCoder <soumyadip.edu2008@gmail.com>
2026-05-31 07:53:16 +0530

    Added array_problem.cpp

--- array_problem.cpp
+++ array_problem.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
 using namespace std;
+#include <iterator>
+
 
 void print_arr(int arr[],int size){
 	for(int i=0;i<size;i++){
@@ -7,8 +9,6 @@ void print_arr(int arr[],int size){
 
 	}
 }
-
-
 bool linear_search(int element,int arr[],int size){
 	for(int i=0; i < size; i++){
 		if(arr[i]==element){
@@ -17,7 +17,6 @@ bool linear_search(int element,int arr[],int size){
 	}
 	return false;
 }
-
 void swap_arr(int arr[],int size){
 	int a;
 	for(int i=1;i<size;i=i+2){
@@ -27,9 +26,35 @@ void swap_arr(int arr[],int size){
 	}
 }
 
+void find_duplicate(int arr[],int size){
+	for(int i = 0; i < size; i++) {
+        for(int j = i + 1; j < size; j++) {
+
+            if(arr[i] == arr[j]) {
+                cout << arr[i] << " ";
+         
+            }
+
+        }
+    }
+}
+
+void intersection(int arr1[],int arr2[],int size1,int size2){
+	cout<<"Intersect elements are:-";
+	for(int i=0;i<size1;i++){
+		for(int j=0;j<size2;j++){
+			if(arr1[i]==arr2[j]){
+				cout<<arr1[i]<<" ";
+			}
+		}
+	}
+}
+
+
+
 
 int main(){
-	int arr[]={10,20,30,40,15,29,148};
+	int arr[]={1,2,5,1,5,10};
 /*
 	//linear search
 	int element;
@@ -44,9 +69,21 @@ int main(){
 	} 
 */
 
- 	//swapint alternates{1,2,3,4,5,6} =>{2,1,4,3,6,5}
+/* 	//swapint alternates{1,2,3,4,5,6} =>{2,1,4,3,6,5}
  	swap_arr(arr,sizeof(arr)/sizeof(arr[0]));
  	print_arr(arr,sizeof(arr)/sizeof(arr[0]));
+*/
+
+//	find_duplicate(arr,sizeof(arr)/sizeof(arr[0]));
+
+
+/*	int arr1[]={10,30,45,75,12};
+	int arr2[]={20,30,10,45};
+	intersection(arr1,arr2,sizeof(arr1)/sizeof(arr1[0]),sizeof(arr2)/sizeof(arr2[0]));
+*/
+
+
+
 
 
 	return 0;
