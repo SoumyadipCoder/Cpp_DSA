0ccd3c7700c586258791b3cff1a4666a2dc1fb62
SoumyadipCoder <soumyadip.edu2008@gmail.com>
2026-07-16 07:57:37 +0530

    Add prime number cheak

--- basic_maths/prime_number.cpp
+++ basic_maths/prime_number.cpp
@@ -1,11 +1,22 @@
  #include<iostream>
 using namespace std;
 
+int prime_cheak(int number){
+	for(int i=2;i<number;i++){
+		if(number%i==0){
+			cout<<"Number is not prime.";
+			return 0;
+		}
+	}
+	cout<<"Number is prime";
+	return 0;
+}
 
 int main(){
 	int number;
 	cout<<"Enter number:-";
 	cin>>number;
 
-	for(int i=0;i<)
+	prime_cheak(number);
+
 }
